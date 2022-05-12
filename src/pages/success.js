ee308793c52707fbca64ef829b1e9ff8daaa88a4
Hill Onyechekwa <hillonyechekwa@gmail.com>
2022-05-12 05:13:19 +0100

    left some things out...

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {Link} from 'gatsby'
 
-const Success = ({location}) => {
+const Success = () => {
     return(
         <main>
             <h1>Success!</h1>
-            <p>Hi {location.state.name || ""}, You are awesome for subscribing to my newsletter!</p>
+            <p> You are awesome for subscribing to my newsletter!</p>
             <p>You are almost done. Check your inbox for a confirmation email</p>
             <p>You can <Link to="/">go back </Link> to reading more posts now</p>
         </main>
